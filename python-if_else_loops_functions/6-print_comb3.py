377c5e309b6a2f9f36ddc665bcc91b83ad55ae83
balemansteve <balemansteve@gmail.com>
2024-05-27 21:26:27 +0000

    6-print_comb3.py fix

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for i in range(0, 8):
         if n > i:
             print("{}{}".format(i, n), end=', ')
 for i in range(8, 10):
-    for n in range(0,10):
+    for n in range(0, 10):
         if n > i:
             print("{}{}".format(i, n))
