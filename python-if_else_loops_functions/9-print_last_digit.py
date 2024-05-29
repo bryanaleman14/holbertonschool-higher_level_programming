2bd9b3b48d038ddcd5e3647875ac6ab525fab4f7
balemansteve <balemansteve@gmail.com>
2024-05-29 05:10:03 +0000

    9-print_last_digit.py fix

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    number_str = str(number)
-    print("{}".format(number_str[-1:]), end='')
-    return number_str[-1:]
+    if isinstance(number, str):
+        return -1
+    else:
+        number_str = str(number)
+        print("{}".format(number_str[-1:]), end='')
+        return number_str[-1:]
