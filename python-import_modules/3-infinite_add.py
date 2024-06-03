a81f242e1757cf1321d1cf8c14cddba68f64fe3e
balemansteve <balemansteve@gmail.com>
2024-06-03 05:04:05 +0000

    3-infinite_add.py

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+    args = sys.argv[1:]
+    suma = 0
+
+    for i in args:
+        suma += int(i)
+    print("{}".format(suma))
+
+
+if __name__ == "__main__":
+    main()
