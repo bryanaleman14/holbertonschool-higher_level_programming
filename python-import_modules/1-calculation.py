4d39e74955ddc250dd105051f05473e3269913b7
balemansteve <balemansteve@gmail.com>
2024-06-03 01:36:41 +0000

    1-calculation.py

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("add: {}".format(add(a, b)))
+    print("sub: {}".format(sub(a, b)))
+    print("mul: {}".format(mul(a, b)))
+    print("div: {}".format(div(a, b)))
