bb950e6f547d56bd8010c75b394c2592a51aec41
balemansteve <balemansteve@gmail.com>
2024-06-03 00:54:05 +0000

    0-add.py condition fix

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 from add_0 import add
 if __name__ == "__main__":
-    a = 1
-    b = 2
-    print("{}".format(add(a, b)))
+    if add(1, 2):
+        a = 1
+        b = 2
+        print("{}".format(add(a, b)))
+    else:
+        print("{}".format(add(a, b)))
