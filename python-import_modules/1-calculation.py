718b0fe59dd362c2d55c88226f696cbb00d89b1d
balemansteve <balemansteve@gmail.com>
2024-06-03 01:39:34 +0000

    1-calculation.py fix

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("add: {}".format(add(a, b)))
-    print("sub: {}".format(sub(a, b)))
-    print("mul: {}".format(mul(a, b)))
-    print("div: {}".format(div(a, b)))
+    print("{}".format(add(a, b)))
+    print("{}".format(sub(a, b)))
+    print("{}".format(mul(a, b)))
+    print("{}".format(div(a, b)))
