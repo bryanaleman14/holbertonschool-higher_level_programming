753cc2c471f67e8ffc5c7f1b228afcb55349aeed
balemansteve <balemansteve@gmail.com>
2024-06-05 16:34:28 +0000

    7-add_tuple.py fix whitespace

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,5 +3,5 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     index_max = max(len(tuple_a), len(tuple_b))
-    tuple_c = tuple((tuple_a[i] if i < len(tuple_a) else 0) + (tuple_b[i] if i < len(tuple_b) else 0)  for i in range(index_max))
+    tuple_c = tuple((tuple_a[i] if i < len(tuple_a) else 0) + (tuple_b[i] if i < len(tuple_b) else 0) for i in range(index_max))
     return tuple_c
