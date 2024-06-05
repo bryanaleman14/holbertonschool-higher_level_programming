20fd4202210e38f28f3236d5f253ef81a04bd632
balemansteve <balemansteve@gmail.com>
2024-06-05 16:38:54 +0000

    7-add_tuple.py condition add

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,5 +3,7 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     index_max = max(len(tuple_a), len(tuple_b))
+    if index_max > 2:
+        index_max == 2
     tuple_c = tuple((tuple_a[i] if i < len(tuple_a) else 0) + (tuple_b[i] if i < len(tuple_b) else 0) for i in range(index_max))
     return tuple_c
