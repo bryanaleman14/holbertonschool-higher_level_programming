67066109600edfee51826d44a2d4222d7cee5b85
balemansteve <balemansteve@gmail.com>
2024-06-09 19:20:23 -0300

    0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+def read_file(filename=""):
+    try:
+        with open(filename, encoding='utf-8') as f:
+            print(f.read(), end="")
+    except FileNotFoundError:
+        pass
