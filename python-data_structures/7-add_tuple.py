539dbb83cbc42607cf933ee8865697887252e6e2
balemansteve <balemansteve@gmail.com>
2024-06-09 00:28:06 -0300

    logic fix

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,6 +2,17 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    index_min = min(len(tuple_a), len(tuple_b))
-    tuple_c = tuple(tuple_a[i] + tuple_b[i] for i in range(index_min))
-    return tuple_c
+    
+    if len(tuple_a) < 2:
+        tuple_a = tuple_a + (0,) * (2 - len(tuple_a))
+    if len(tuple_b) < 2:
+        tuple_b = tuple_b + (0,) * (2 - len(tuple_b))
+    
+   
+    tuple_a = tuple_a[:2]
+    tuple_b = tuple_b[:2]
+    
+    
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    
+    return result
