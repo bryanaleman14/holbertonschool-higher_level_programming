6d971ebad2a4952cce0a851d42cfb2b4dfa92ad5
balemansteve <balemansteve@gmail.com>
2024-06-09 01:50:04 -0300

    9-max_integer.py

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    max_value = 0
+    if len(my_list) == 0:
+        return None
+    else:
+        for i in my_list:
+            if i is None or i > max_value:
+                max_value = i
+        return max_value
