b984e374c9a51d61a50159df866acbc769015c87
balemansteve <balemansteve@gmail.com>
2024-06-09 00:35:34 -0300

    style fix

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,10 +3,10 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
 
-    if len(tuple_a) < 2:
-        tuple_a = tuple_a + (0,) * (2 - len(tuple_a))
-    if len(tuple_b) < 2:
-        tuple_b = tuple_b + (0,) * (2 - len(tuple_b))
+	if len(tuple_a) < 2:
+		tuple_a = tuple_a + (0,) * (2 - len(tuple_a))
+	if len(tuple_b) < 2:
+		tuple_b = tuple_b + (0,) * (2 - len(tuple_b))
 
 
 	tuple_a = tuple_a[:2]
